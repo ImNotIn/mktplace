f87925b059f73e7f0b5848752cf87d755ed6227a
ImNotIn <67018726+ImNotIn@users.noreply.github.com>
2020-06-16 15:43:39 -0300

    .

--- shoping-cart.php
+++ shoping-cart.php
@@ -1,26 +1,5 @@
 <?php include 'menu_layout.php';  ?>
 
-
-<!-- Categories Section Begin -->
-
-    <!-- Breadcrumb Section Begin  Comentado Barra do carrinho de compras onde mostra a img e titulo--> 
-    <!-- <section class="breadcrumb-section set-bg" data-setbg="img/breadcrumb.jpg">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 text-center">
-                    <div class="breadcrumb__text">
-                        <h2>Carrinho de Compras</h2>
-                        <div class="breadcrumb__option">
-                            <a href="./index.html">Home</a>
-                            <span>Carrinho de Compras</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> -->
-    <!-- Breadcrumb Section End -->
-
     <!-- Shoping Cart Section Begin -->
     <section class="shoping-cart spad">
         <div class="container">
